b2f145af3b2834a3f23c0d367cbe5f2775017e5a
Bruno Rocha <rochacbruno@gmail.com>
2013-07-10 03:10:42 -0300

    Better creation of fileadmin instances

--- quokka/admin/__init__.py
+++ quokka/admin/__init__.py
@@ -4,10 +4,6 @@ from flask.ext.superadmin import Admin
 from flask.ext.superadmin.contrib import fileadmin
 
 
-def file_admin_factory(name):
-    return type(name.capitalize(), (fileadmin.FileAdmin,), {})
-
-
 def create_admin(app):
 
     SUPER_ADMIN = app.config.get(
@@ -20,15 +16,14 @@ def create_admin(app):
 
     admin = Admin(app, **SUPER_ADMIN)
 
-    # EXCEPTION: UndefinedError: 'flask_superadmin.
-    # contrib.fileadmin.FileAdmin object' has no attribute 'field_name'
     for entry in app.config.get('FILE_ADMIN', []):
         admin.add_view(
-            file_admin_factory(entry['name'])(
+            fileadmin.FileAdmin(
                 entry['path'],
                 entry['url'],
                 name=entry['name'],
-                category=entry['category']
+                category=entry['category'],
+                endpoint=entry['name']
             )
         )
 
