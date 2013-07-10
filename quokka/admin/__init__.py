7196b03822bb06d932831bf49e8f9fa358e55ff5
Bruno Rocha <rochacbruno@gmail.com>
2013-07-10 00:54:56 -0300

    Added fileadmin, but found a bug in Flask SuperAdmin

--- quokka/admin/__init__.py
+++ quokka/admin/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
-
+import os
 from flask.ext.superadmin import Admin
+from flask.ext.superadmin.contrib import fileadmin
 
 
 def create_admin(app):
@@ -15,5 +16,14 @@ def create_admin(app):
     )
 
     admin = Admin(app, **SUPER_ADMIN)
+
+    # TODO: Add multiple instances of fileadmin
+    # EXCEPTION: UndefinedError: 'flask_superadmin.contrib.fileadmin.FileAdmin object' has no attribute 'field_name'
+    # admin.add_view(fileadmin.FileAdmin(app.static_folder, '/staticfiles', name='Static'))
+    admin.add_view(fileadmin.FileAdmin(
+        os.path.join(app.config.get('PROJECT_ROOT'), app.template_folder),
+        '/templatefiles',
+        name='template_files')
+    )
 
     return admin
--- quokka/settings.py
+++ quokka/settings.py
@@ -5,6 +5,8 @@ MONGODB_SETTINGS = {'DB': "quokka_1"}
 SECRET_KEY = "KeepThisS3cr3t"
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 BLUEPRINTS_PATH = 'blueprints'
 BLUEPRINTS_OBJECT_NAME = 'module'
 SUPER_ADMIN = {'name': 'Quokka admin', 'url': '/admin'}
