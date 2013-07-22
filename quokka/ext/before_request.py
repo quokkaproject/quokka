e6fcb5122b7132e03257ac5c883f5e44ccdd1ef5
Bruno Rocha <rochacbruno@gmail.com>
2013-07-22 18:10:54 -0300

    Create channel homepage if not exists in before request

--- quokka/ext/before_request.py
+++ quokka/ext/before_request.py
@@ -1,7 +1,23 @@
 # coding: utf-8
 
+from quokka.core.models import Channel
+
 
 def configure(app):
     @app.before_first_request
     def initialize():
         print "Called only once, when the first request comes in"
+        if not Channel.objects.count():
+            # Create homepage if it does not exists
+            Channel.objects.create(
+                title="home",
+                slug="home",
+                description="App homepage",
+                is_homepage=True,
+                include_in_rss=True,
+                indexable=True,
+                show_in_menu=True,
+                canonical_url="/",
+                order=0,
+                published=True,
+            )
