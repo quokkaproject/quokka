cf9a4dc9527ff983cc4d3c3ffb8b7e6360e4c7a7
Bruno Rocha <rochacbruno@gmail.com>
2013-10-15 22:02:05 -0300

    Added deployment scripts for nginx & uwsgi. Thanks @jniltinho

---
+++ quokka/contrib/scripts/setup-quokka-nginx-uwsgi-opensuse.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+# Autor: Nilton OS -- www.linuxpro.com.br
+echo 'setup-quokka-nginx-uwsgi-opensuse.sh'
+echo 'Support OpenSUSE  11.X, 12.X, 13.X'
+echo 'Installs Nginx + uWSGI + Quokka'
+ 
+SUSE_VERSION=$(cat /etc/issue | awk '{ print $4 }' | head -n1)
+ 
+echo -e "Set Server Name Ex: quokkaproject.domain.com : \c "
+read  SERVER_FQDN
+ 
+echo -e "Set Server IP: \c "
+read  SERVER_IP
+ 
+ 
+echo "" >>/etc/hosts
+echo "$SERVER_IP  $SERVER_FQDN" >>/etc/hosts
+ 
+ 
+zypper ar http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_${SUSE_VERSION}/ devel_python
+zypper --no-gpg-checks refresh
+zypper in -y gcc make python-devel python-pip mongodb git-core python-virtualenv sudo nginx
+
+## Start MongoDB
+/etc/init.d/mongodb start
+chkconfig --add mongodb
+
+## Create VirtualEnv and User Quokka
+groupadd quokka
+useradd -m -G quokka --system -s /bin/sh -c 'Quokka' -d /home/quokka quokka
+cd /home/quokka
+sudo -u quokka -H virtualenv quokka-env
+cd quokka-env
+source bin/activate
+sudo -u quokka -H git clone https://github.com/rochacbruno/quokka
+cd quokka
+sudo -u quokka -H pip install -r requirements.txt
+
+
+chown -R quokka:quokka /home/quokka
+
+## Populating with sample data
+python manage.py populate
+
+
+PIPPATH=`which pip`
+$PIPPATH install --upgrade uwsgi
+  
+# Prepare folders for uwsgi
+mkdir /etc/uwsgi
+mkdir /var/log/uwsgi
+ 
+usermod -a -G www nginx
+ 
+mkdir -p /etc/nginx/vhosts.d/
+mkdir -p /etc/nginx/ssl/
+
+ 
+ 
+echo 'server {
+        listen          YOUR_SERVER_IP:80;
+        server_name     YOUR_SERVER_FQDN;
+        location / {
+            uwsgi_pass      unix:///tmp/quokka.socket;
+            include         /etc/nginx/uwsgi_params;
+            uwsgi_param     UWSGI_SCHEME $scheme;
+            uwsgi_param     SERVER_SOFTWARE    nginx/$nginx_version;
+
+            ### remove the comments if you use uploads (max 10 MB)
+            #client_max_body_size 10m;
+            ###
+        }
+}' >/etc/nginx/vhosts.d/quokka.conf
+ 
+ 
+sed -i "s/YOUR_SERVER_IP/$SERVER_IP/" /etc/nginx/vhosts.d/quokka.conf
+sed -i "s/YOUR_SERVER_FQDN/$SERVER_FQDN/" /etc/nginx/vhosts.d/quokka.conf
+
+
+# Create configuration file /etc/uwsgi/quokka.ini
+echo '[uwsgi]
+chmod-socket = 666
+chdir = /home/quokka/quokka-env/quokka
+virtualenv = /home/quokka/quokka-env
+module = wsgi
+socket = /tmp/%n.socket
+socket = /tmp/%n.sock
+logto = /var/log/uwsgi/%n.log
+workers = 3
+uid = quokka
+gid = quokka' >/etc/uwsgi/quokka.ini
+ 
+ 
+ 
+ 
+## Daemons /start/stop
+ 
+echo '#!/bin/sh
+# Autor: Nilton OS -- www.linuxpro.com.br
+#
+#
+### BEGIN INIT INFO
+# Provides:          uwsgi
+# Required-Start:    $syslog $remote_fs
+# Should-Start:      $time ypbind smtp
+# Required-Stop:     $syslog $remote_fs
+# Should-Stop:       ypbind smtp
+# Default-Start:     3 5
+# Default-Stop:      0 1 2 6
+# Short-Description: Application Container Server for Networked/Clustered Web Applications
+# Description:       Application Container Server for Networked/Clustered Web Applications
+### END INIT INFO
+ 
+# Check for missing binaries (stale symlinks should not happen)
+UWSGI_BIN=`which uwsgi`
+test -x $UWSGI_BIN || { echo "$UWSGI_BIN not installed"; 
+        if [ "$1" = "stop" ]; then exit 0;
+        else exit 5; fi; }
+ 
+UWSGI_EMPEROR_MODE=true
+UWSGI_VASSALS="/etc/uwsgi/"
+UWSGI_OPTIONS="--logto /var/log/uwsgi/uwsgi.log"
+ 
+ 
+UWSGI_OPTIONS="$UWSGI_OPTIONS --autoload"
+ 
+if [ "$UWSGI_EMPEROR_MODE" = "true" ] ; then
+    UWSGI_OPTIONS="$UWSGI_OPTIONS --emperor $UWSGI_VASSALS"
+fi                
+. /etc/rc.status 
+rc_reset
+ 
+case "$1" in
+    start)
+        echo -n "Starting uWSGI "
+        /sbin/startproc $UWSGI_BIN $UWSGI_OPTIONS
+        rc_status -v
+        ;;
+    stop)
+        echo -n "Shutting down uWSGI "
+        /sbin/killproc $UWSGI_BIN
+        rc_status -v
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        rc_status
+        ;;
+    reload)
+        echo -n "Reload service uWSGI "
+        /sbin/killproc -HUP $UWSGI_BIN
+        rc_status -v
+        ;;
+    status)
+        echo -n "Checking for service uWSGI "
+        /sbin/checkproc $UWSGI_BIN
+        rc_status -v
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|status|restart|reload}"
+        exit 1
+        ;;
+esac
+rc_exit '> /etc/init.d/uwsgi
+ 
+chmod +x /etc/init.d/uwsgi
+ln -s /etc/init.d/uwsgi /usr/sbin/rcuwsgi
+ 
+/etc/init.d/uwsgi start
+/etc/init.d/nginx restart
+ 
+ 
+chkconfig --add uwsgi
+chkconfig --add nginx
+  
+## you can reload uwsgi with
+#/etc/init.d/uwsgi restart
+## to reload Quokka only (without restarting uwsgi)
+# touch /etc/uwsgi/quokka.ini
---
+++ quokka/contrib/scripts/setup-quokka-nginx-uwsgi-ubuntu.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+# Autor: Nilton OS -- www.linuxpro.com.br
+echo 'setup-quokka-nginx-uwsgi-ubuntu.sh'
+echo 'Support Ubuntu/Debian'
+echo 'Installs Nginx + uWSGI + Quokka'
+ 
+ 
+echo -e "Set Server Name Ex: quokkaproject.domain.com : \c "
+read  SERVER_FQDN
+ 
+echo -e "Set Server IP: \c "
+read  SERVER_IP
+ 
+ 
+echo "" >>/etc/hosts
+echo "$SERVER_IP  $SERVER_FQDN" >>/etc/hosts
+ 
+ 
+# Upgrade and install needed software
+apt-get update
+apt-get -y upgrade
+apt-get autoremove
+apt-get autoclean
+apt-get -y install nginx-full mongodb-server git-core python-virtualenv
+apt-get -y install build-essential python-dev libxml2-dev python-pip unzip
+
+
+
+## Create VirtualEnv and User Quokka
+adduser --disabled-login --gecos 'Quokka' quokka
+cd /home/quokka
+sudo -u quokka -H virtualenv quokka-env
+cd quokka-env
+source bin/activate
+sudo -u quokka -H git clone https://github.com/rochacbruno/quokka
+cd quokka
+
+chown -R quokka:quokka /home/quokka
+pip install -r requirements.txt
+chown -R quokka:quokka /home/quokka
+
+## Populating with sample data
+python manage.py populate
+
+deactivate
+## Install uWSGI
+PIPPATH=`which pip`
+$PIPPATH install --upgrade uwsgi
+  
+# Prepare folders for uwsgi
+mkdir /etc/uwsgi
+mkdir /var/log/uwsgi
+ 
+  
+ 
+echo 'server {
+        listen          YOUR_SERVER_IP:80;
+        server_name     YOUR_SERVER_FQDN;
+        location / {
+            uwsgi_pass      unix:///tmp/quokka.socket;
+            include         /etc/nginx/uwsgi_params;
+            uwsgi_param     UWSGI_SCHEME $scheme;
+            uwsgi_param     SERVER_SOFTWARE    nginx/$nginx_version;
+
+            ### remove the comments if you use uploads (max 10 MB)
+            #client_max_body_size 10m;
+            ###
+        }
+}' >/etc/nginx/sites-available/quokka.conf
+ 
+
+ln -s /etc/nginx/sites-available/quokka.conf /etc/nginx/sites-enabled/quokka.conf
+rm /etc/nginx/sites-enabled/default
+mkdir /etc/nginx/ssl
+
+sed -i "s/YOUR_SERVER_IP/$SERVER_IP/" /etc/nginx/sites-available/quokka.conf
+sed -i "s/YOUR_SERVER_FQDN/$SERVER_FQDN/" /etc/nginx/sites-available/quokka.conf
+
+
+# Create configuration file /etc/uwsgi/quokka.ini
+echo '[uwsgi]
+chmod-socket = 666
+chdir = /home/quokka/quokka-env/quokka
+virtualenv = /home/quokka/quokka-env
+module = wsgi
+socket = /tmp/%n.socket
+socket = /tmp/%n.sock
+logto = /var/log/uwsgi/%n.log
+workers = 3
+uid = quokka
+gid = quokka' >/etc/uwsgi/quokka.ini
+ 
+ 
+ #Create a configuration file for uwsgi in emperor-mode
+#for Upstart in /etc/init/uwsgi-emperor.conf
+echo '# Emperor uWSGI script
+
+description "uWSGI Emperor"
+start on runlevel [2345]
+stop on runlevel [06]
+##
+#remove the comments in the next section to enable static file compression for the welcome app
+#in that case, turn on gzip_static on; on /etc/nginx/nginx.conf
+##
+#pre-start script
+#    chown -R quokka:quokka /home/quokka/*
+#end script
+respawn
+exec uwsgi --master --die-on-term --emperor /etc/uwsgi --logto /var/log/uwsgi/uwsgi.log
+' > /etc/init/uwsgi-emperor.conf
+
+
+start uwsgi-emperor
+/etc/init.d/nginx restart
+
+## you can reload uwsgi with
+# restart uwsgi-emperor
+## and stop it with
+# stop uwsgi-emperor
+## to reload quokka only (without restarting uwsgi)
+# touch /etc/uwsgi/quokka.ini
