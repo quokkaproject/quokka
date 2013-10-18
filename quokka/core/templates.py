cfeb02096ee4e058c3afe4a82da15fd357180216
Bruno Rocha <rochacbruno@gmail.com>
2013-10-18 01:50:16 -0300

    render_template can receive a list of themes

--- quokka/core/templates.py
+++ quokka/core/templates.py
@@ -4,6 +4,13 @@ from flask import session, current_app
 from quokka_themes import render_theme_template
 
 
-def render_template(template, **context):
-    theme = session.get('theme', current_app.config.get('DEFAULT_THEME'))
+def render_template(template, theme=None, **context):
+    theme = theme or []
+    if not isinstance(theme, (list, tuple)):
+        theme = [theme]
+
+    sys_theme = session.get('theme', current_app.config.get('DEFAULT_THEME'))
+    if sys_theme:
+        theme.append(sys_theme)
+
     return render_theme_template(theme, template, **context)
