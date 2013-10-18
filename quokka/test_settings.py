eba8fc2ba9e441ca034b37e80f83ecf5bf80c017
Bruno Rocha <rochacbruno@gmail.com>
2013-10-18 01:53:26 -0300

    templates got its name and suffix based in template_type

--- quokka/test_settings.py
+++ quokka/test_settings.py
@@ -6,7 +6,12 @@ This file is used for runtests.py or make test
 
 from quokka.settings import *
 
-MONGODB_SETTINGS = {'DB': "quokka_test"}  # use in localhost
+# MONGODB_SETTINGS = {'DB': "quokka_test"}  # use in localhost
+
+MONGODB_SETTINGS = {'DB': 'quokka_local',
+                    'USERNAME': 'quokka',
+                    'PASSWORD': '302010'}
+
 MODE = 'testing'
 DEBUG = False
 DEBUG_TOOLBAR_ENABLED = False
