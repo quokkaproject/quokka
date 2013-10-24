cc6aa8525c0586817730035c4b35ca6cdd3b7c10
Bruno Rocha <rochacbruno@gmail.com>
2013-10-24 20:13:15 -0200

    implemented lazy_media_path and media_path for uploads

--- quokka/utils/upload.py
+++ quokka/utils/upload.py
@@ -11,23 +11,26 @@ def dated_path(obj, file_data):
     try:
         prefix = getattr(obj, 'model_name')
     except:
-        prefix = ""
+        prefix = "undefined"
 
     parts = op.splitext(file_data.filename)
     rand = random.getrandbits(16)
-    filename = u"{prefix}_{rand}_{name}{ext}".format(
-        prefix=prefix, rand=rand, name=parts[0], ext=parts[1]
+    filename = u"{name}_{rand}{ext}".format(
+        rand=rand, name=parts[0], ext=parts[1]
     )
     filename = secure_filename(filename)
     today = date.today()
-    path = u"{t.year}/{t.month}/{filename}".format(
-        t=today, filename=filename, r=random.getrandbits(32)
+    path = u"{prefix}/{t.year}/{t.month}/{filename}".format(
+        prefix=prefix, t=today, filename=filename, r=random.getrandbits(32)
     )
     return path
 
 
 def media_path(suffix=None):
-    return op.join(current_app.config.get('MEDIA_ROOT'), suffix)
+    media_root = current_app.config.get('MEDIA_ROOT')
+    if suffix:
+        return op.join(media_root, suffix)
+    return op.join(media_root)
 
 
 def lazy_media_path(suffix=None):
