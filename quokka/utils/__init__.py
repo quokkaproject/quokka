bbace90cce62e78edc6791f97c4079ad0282c9b3
Bruno Rocha <rochacbruno@gmail.com>
2013-10-25 00:14:27 -0200

    added lazy_str_settings in utils

--- quokka/utils/__init__.py
+++ quokka/utils/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+from flask import current_app
+from speaklater import make_lazy_string
+
+
+def lazy_str_setting(key, default=None):
+    return make_lazy_string(
+        lambda: current_app.config.get(key, default)
+    )
