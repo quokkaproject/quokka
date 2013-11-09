147cdead6b49cbee8e97ab3c09db73df6392a9f4
Bruno Rocha <rochacbruno@gmail.com>
2013-11-09 19:49:18 -0200

    added isintance template filter

--- quokka/ext/template_filters.py
+++ quokka/ext/template_filters.py
@@ -2,6 +2,15 @@
 
 # TODO load from blueprints
 
+basetypes = (int, str, float, unicode, basestring, dict, list, tuple)
+
+
+def is_instance(v, cls):
+    cls_map = {
+        tp.__name__: tp for tp in basetypes
+    }
+    return isinstance(v, cls_map.get(cls, str))
+
 
 def configure(app):
-    pass
+    app.jinja_env.filters['isinstance'] = is_instance
