5a3d82da40d341b83e5dcb4c615a8cfee343e2d5
Bruno Rocha <rochacbruno@gmail.com>
2013-11-17 05:12:02 -0200

    using app request context manager for testing

--- quokka/__init__.py
+++ quokka/__init__.py
@@ -38,8 +38,10 @@ def create_app(config=None, test=False, admin_instance=None, **settings):
     else:
         app.config.from_envvar("QUOKKATEST_SETTINGS", silent=True)
 
-    from .ext import configure_extensions
-    configure_extensions(app, admin_instance or admin)
+    # testing trick
+    with app.test_request_context():
+        from .ext import configure_extensions
+        configure_extensions(app, admin_instance or admin)
 
     # app.wsgi_app = HTTPMethodOverrideMiddleware(app.wsgi_app)
     return app
