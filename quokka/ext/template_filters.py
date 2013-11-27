057b8f45897df601dedf291c198a7035eb604362
Bruno Rocha <rochacbruno@gmail.com>
2013-11-27 14:18:24 -0200

    Added template globals to get filtered content

--- quokka/ext/template_filters.py
+++ quokka/ext/template_filters.py
@@ -5,6 +5,7 @@
 from flask import Blueprint
 from werkzeug.routing import Rule
 from quokka.core.app import QuokkaModule
+from quokka.core.models import Content
 from quokka_themes import Theme
 
 
@@ -19,5 +20,21 @@ def is_instance(v, cls):
     return isinstance(v, cls_map.get(cls, str))
 
 
+def get_content(**kwargs):
+    try:
+        return Content.objects.get(**kwargs)
+    except:
+        return None
+
+
+def get_contents(limit=None, order_by="-created_at", **kwargs):
+    contents = Content.objects.filter(**kwargs).order_by(order_by)
+    if limit:
+        contents = contents[:limit]
+    return contents
+
+
 def configure(app):
     app.jinja_env.filters['isinstance'] = is_instance
+    app.add_template_global(get_content)
+    app.add_template_global(get_contents)
