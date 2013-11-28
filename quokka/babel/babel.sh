176c182d111085bfac07224d34bea510e69796d3
Xiao Hanyu <xiaohanyu1988@gmail.com>
2013-11-28 23:49:02 +0800

    Fix wrong directory in babel script.
    Then we can just "cd babel; bash babel.sh".

--- quokka/babel/babel.sh
+++ quokka/babel/babel.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pybabel extract -F babel.ini -k _gettext -k _ngettext -k lazy_gettext -o quokka.pot --project quokka ../quokka
+pybabel extract -F babel.ini -k _gettext -k _ngettext -k lazy_gettext -o quokka.pot --project quokka ..
