f90883b2b4f93808c96343192b86afddd05c4fa4
Bruno Rocha <rochacbruno@gmail.com>
2013-12-01 05:27:05 -0200

    added ThumbWidget, ThumbField and ContentImageWidget

--- quokka/core/admin/fields.py
+++ quokka/core/admin/fields.py
@@ -1,6 +1,36 @@
 #coding: utf-8
+import random
+
+from werkzeug.datastructures import FileStorage
 from flask import current_app
 from flask.ext.admin import form
+from flask.ext.admin.form.upload import ImageUploadInput
+from flask.ext.admin._compat import urljoin
+
+from quokka.core.models import SubContent, SubContentPurpose
+from quokka.modules.media.models import Image
+
+
+class ThumbWidget(ImageUploadInput):
+    empty_template = ""
+    data_template = ('<div class="image-thumbnail">'
+                     ' <img %(image)s>'
+                     '</div>')
+
+    def get_url(self, field):
+        if field.thumbnail_size:
+            filename = field.thumbnail_fn(field.data)
+        else:
+            filename = field.data
+
+        if field.url_relative_path:
+            filename = urljoin(field.url_relative_path, filename)
+
+        return field.data
+
+
+class ThumbField(form.ImageUploadField):
+    widget = ThumbWidget()
 
 
 class ImageUploadField(form.ImageUploadField):
@@ -10,3 +40,49 @@ class ImageUploadField(form.ImageUploadField):
             items = current_app.config.get(extensions, extensions)
             self.allowed_extensions = items
         return super(ImageUploadField, self).is_file_allowed(filename)
+
+
+class ContentImageField(ImageUploadField):
+    def populate_obj(self, obj, name):
+        pass
+
+    def save_contents(self, obj):
+        # field = getattr(obj, name, None)
+        # if field:
+        #     # If field should be deleted, clean it up
+        #     if self._should_delete:
+        #         self._delete_file(field)
+        #         setattr(obj, name, None)
+        #         return
+
+        new_image = Image(
+            title=u"Image: {0}".format(obj.title),
+            slug=u"{0}-{1}".format(obj.slug, random.getrandbits(8)),
+            channel=Image.get_default_channel(),
+            published=True
+        )
+        if self.data and isinstance(self.data, FileStorage):
+            # if field:
+            #     self._delete_file(field)
+
+            filename = self.generate_name(new_image, self.data)
+            filename = self._save_file(self.data, filename)
+
+            setattr(new_image, 'path', filename)
+
+            new_image.save()
+
+            if obj.contents.filter(identifier='mainimage'):
+                purpose = SubContentPurpose.objects.get(identifier='image')
+            else:
+                purpose = SubContentPurpose.objects.get(
+                    identifier='mainimage'
+                )
+
+            subcontent = SubContent(
+                content=new_image,
+                purpose=purpose,
+            )
+
+            obj.contents.append(subcontent)
+            obj.save()
