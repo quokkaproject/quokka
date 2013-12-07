bcfcca9fa0a7e08c47bb665ff8c30ff007d78436
Bruno Rocha <rochacbruno@gmail.com>
2013-12-07 00:29:23 -0200

    disabled template panel on toolbar (due to unicode problems)

--- quokka/settings.py
+++ quokka/settings.py
@@ -136,7 +136,7 @@ DEBUG_TB_PANELS = (
     'flask_debugtoolbar.panels.timer.TimerDebugPanel',
     'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
     'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
-    'flask_debugtoolbar.panels.template.TemplateDebugPanel',
+    #'flask_debugtoolbar.panels.template.TemplateDebugPanel',
     #'flask.ext.mongoengine.panels.MongoDebugPanel',
     'flask_debugtoolbar.panels.logger.LoggingPanel',
     'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel',
