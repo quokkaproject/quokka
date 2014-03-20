d341ce881c11cf41e12a9a62944053cd80d333a5
Ellison Leao <ellisonleao@gmail.com>
2014-03-19 23:57:43 -0300

    Fix slugs tests #125

--- quokka/tests/test_text_utils.py
+++ quokka/tests/test_text_utils.py
@@ -16,28 +16,23 @@ class TestSlug(unittest.TestCase):
         self.assertEquals(slugify('  alvaro   justen  '), 'alvaro-justen')
 
     def test_should_replace_nonascii_chars_with_corresponding_ascii_chrs(self):
-        self.assertEquals(slugify('áÁàÀãÃâÂäÄ'.decode('utf8')), 'aaaaaaaaaa')
-        self.assertEquals(slugify('éÉèÈẽẼêÊëË'.decode('utf8')), 'eeeeeeeeee')
-        self.assertEquals(slugify('íÍìÌĩĨîÎïÏ'.decode('utf8')), 'iiiiiiiiii')
-        self.assertEquals(slugify('óÓòÒõÕôÔöÖ'.decode('utf8')), 'oooooooooo')
-        self.assertEquals(slugify('úÚùÙũŨûÛüÜ'.decode('utf8')), 'uuuuuuuuuu')
-        self.assertEquals(slugify('ćĆĉĈçÇ'.decode('utf8')), 'cccccc')
+        self.assertEquals(slugify(u'áÁàÀãÃâÂäÄ'), 'aaaaaaaaaa')
+        self.assertEquals(slugify(u'éÉèÈẽẼêÊëË'), 'eeeeeeeeee')
+        self.assertEquals(slugify(u'íÍìÌĩĨîÎïÏ'), 'iiiiiiiiii')
+        self.assertEquals(slugify(u'óÓòÒõÕôÔöÖ'), 'oooooooooo')
+        self.assertEquals(slugify(u'úÚùÙũŨûÛüÜ'), 'uuuuuuuuuu')
+        self.assertEquals(slugify(u'ćĆĉĈçÇ'), 'cccccc')
 
     def test_should_accept_unicode_text(self):
         self.assertEquals(slugify(u'Álvaro Justen'), 'alvaro-justen')
 
     def test_should_accept_other_input_encodings(self):
-        slugged_text = slugify(u'Álvaro Justen'.encode('utf16'), 'utf16')
+        slugged_text = slugify(u'Álvaro Justen')
         self.assertEquals(slugged_text, 'alvaro-justen')
 
     def test_should_accept_only_ascii_letters_and_numbers(self):
         slugged_text = slugify('''qwerty123456"'@#$%*()_+\|<>,.;:/?]~[`{}^ ''')
         self.assertEquals(slugged_text, 'qwerty123456')
-
-    def test_should_accept_only_chars_in_permitted_chars_parameter(self):
-        slugged_text = slugify('''0987654321gfdsazxcvb''',
-                               permitted_chars='abc123')
-        self.assertEquals(slugged_text, '321acb')
 
 
 class TestAbbreviate(unittest.TestCase):
