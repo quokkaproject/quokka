5ba4207ec99fa9dd0dba2d379fee2971f0cda132
Ellison Leao <ellisonleao@gmail.com>
2014-03-20 02:00:09 -0300

    Change Misaka to Mistune #133

--- quokka/ext/generic.py
+++ quokka/ext/generic.py
@@ -1,21 +1,12 @@
 # coding: utf-8
 
-#from flaskext.markdown import Markdown
-from flask.ext.misaka import Misaka
+from flask.ext.mistune import Mistune
 from flask_wtf.csrf import CsrfProtect
 
 
 def configure(app):
     # Markdown(app)
-    Misaka(app,
-           fenced_code=True,
-           autolink=True,
-           no_intra_emphasis=True,
-           strikethrough=True,
-           superscript=True,
-           tables=True,
-           hard_wrap=True,
-           safelink=True)
+    Mistune(app)
     CsrfProtect(app)
     if app.config.get('GRAVATAR'):
         from flask.ext.gravatar import Gravatar
