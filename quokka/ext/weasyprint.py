225e982ca1c9bba3100774522a7ae9a32eca550d
Blake VandeMerwe <blakev@null.net>
2015-05-13 12:18:18 -0600

    Added #89: Integrate flask-weasyprint

---
+++ quokka/ext/weasyprint.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import collections
+
+from flask import url_for
+
+
+logger = logging.getLogger()
+
+try:
+    from flask_weasyprint import render_pdf, HTML
+
+    import_error = False
+except (ImportError, OSError) as e:
+
+    print ("""
+    Error importing flask-weasyprint!
+    PDF support is temporarily disabled.
+    Manual dependencies may need to be installed.
+    See,
+        `http://weasyprint.org/docs/install/#by-platform`_
+        `https://github.com/Kozea/WeasyPrint/issues/79`_
+
+    """ + e.message)
+
+    import_error = True
+
+
+def configure(app):
+    # only configure .pdf extension if it's enabled
+    # and configured correctly in the environment.
+    if app.config.get('ENABLE_TO_PDF', False) and not import_error:
+
+        def render_to_pdf(long_slug):
+            return render_pdf(url_for('detail', long_slug=long_slug))
+
+        app.add_url_rule('/<path:long_slug>.pdf', view_func=render_to_pdf)
