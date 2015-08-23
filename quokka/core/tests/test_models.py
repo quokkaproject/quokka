48d74a201c87392250c4607d79705f7722f7ebe4
Bruno Rocha <rochacbruno@gmail.com>
2015-08-22 22:32:07 -0300

    fix test

--- quokka/core/tests/test_models.py
+++ quokka/core/tests/test_models.py
@@ -3,7 +3,8 @@ import sys
 
 from . import BaseTestCase
 
-from ..models import Channel, Config, CustomValue
+from ..models import Channel, Config
+from ..base_models.custom_values import CustomValue
 
 if sys.version_info.major == 3:
     unicode = lambda x: u'{}'.format(x)
