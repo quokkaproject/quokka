02fbd1d8a02c737165a63cc515d3f4de6c7f60f7
Bruno Rocha <rochacbruno@gmail.com>
2015-08-23 23:48:48 -0300

    mongoengine cant work with pymongo3.0 yet

--- etc/openshift_clean.py
+++ etc/openshift_clean.py
@@ -5,13 +5,14 @@ RUN ONLY IN OPENSHIFT DEMO DEPLOY
 OR AT YOUR OWN RISK!!!!
 """
 from quokka import create_app
-from quokka.core.models import Content, Channel
+from quokka.core.models import Content, Channel, Config
 from quokka.modules.accounts.models import User
 
 app = create_app()
 
 Content.objects.delete()
 User.objects.delete()
+Config.objects.delete()
 
 for channel in Channel.objects.filter(parent__ne=None):
     channel.delete()
