65dd837d24a3cc788908ca0abf351ac486a3f780
Bruno Rocha <rochacbruno@gmail.com>
2017-03-19 03:34:29 -0300

    Starting to write the Base Content Form

--- quokka/core/content.py
+++ quokka/core/content.py
@@ -3,27 +3,38 @@
 from quokka.db import collection_index
 from quokka.admin.views import ModelView
 from quokka.admin.forms import fields, Form
+from flask_admin.model.fields import InlineFieldList, InlineFormField
+from flask_admin.form import Select2Widget
+from wtforms import validators
 
 
 class ContentForm(Form):
     """Base form for all contents"""
 
-    title = fields.StringField('title')
-    summary = fields.StringField('summary')
-    category = fields.StringField('category')
-    tags = fields.StringField('tags')
-    slug = fields.StringField('slug')
+    title = fields.StringField('Title')
+    summary = fields.TextAreaField('Summary')
+    category = fields.StringField('Category')
+    tags = fields.StringField('Tags')
+    slug = fields.StringField('Slug')
 
-    authors = fields.StringField('authors')
-    date = fields.StringField('date')
-    modified = fields.StringField('modified')
+    # authors = fields.StringField('authors')
+    # authors = fields.FieldList(
+    #    fields.StringField('authors', [validators.required()]))
 
-    lang = fields.StringField('lang')
-    translations = fields.StringField('translations')
-    status = fields.StringField('status')
+    authors = InlineFieldList(fields.StringField('Authors'))
+
+    date = fields.DateTimeField('Date')
+    modified = fields.HiddenField('Modified')
+
+    lang = fields.StringField('Language')
+    translations = fields.HiddenField('Translations')
+    status = fields.HiddenField('status')
 
     # metadata
-    content_type = fields.StringField('type')
+    content_type = fields.SelectField(
+        'Type',
+        widget=Select2Widget()
+    )
 
 
 class ContentView(ModelView):
@@ -38,6 +49,11 @@ class ContentView(ModelView):
         'lang',
         'status'
     )
+
+    def create_form(self):
+        form = super(ContentView, self).create_form()
+        form.content_type.choices = [('a', 'a'), ('b', 'b')]
+        return form
 
 
 def configure(app, db, admin):
--- quokka/db.py
+++ quokka/db.py
@@ -1,10 +1,19 @@
 from tinymongo import TinyMongoClient
+from tinydb_serialization import SerializationMiddleware
+from tinymongo.serializers import DateTimeSerializer
+
 from quokka.config import settings
 
 db_system = settings.get('db_system', 'tinydb')
 
 if db_system == 'tinydb':
-    connection = TinyMongoClient(settings.get('db_folder', 'databases'))
+    serialization = SerializationMiddleware()
+    serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
+    # TODO: Read custom serializers from settings and extensions
+    connection = TinyMongoClient(
+        settings.get('db_folder', 'databases'),
+        storage=serialization
+    )
 elif db_system == 'mongo':
     connection = 'TODO: Load Pymongo here'
 
@@ -19,4 +28,3 @@ collection_contents = db_contents[
     settings.get('collection_contents', 'contents')]
 collection_uploads = db_uploads[settings.get('collection_uploads', 'uploads')]
 collection_users = db_users[settings.get('collection_users', 'users')]
-
