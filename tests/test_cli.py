4f4f3c638408b988db233f934d3d65505ba54376
marcosptf <marcosptf@yahoo.com.br>
2018-06-11 03:39:36 -0300

    add def test_copy_folder_file_exists

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -47,27 +47,27 @@ def test_copy_folder_error_second_param():
             raise
 
 
-#def test_copy_folder_file_exists():
+def test_copy_folder_file_exists():
     
-    #try:
-        #copyfolder(directory_pwd+file_test, directory_pwd+directory_test+file_test)        
-        #assert os.path.isfile(directory_pwd+directory_test+file_test) is True
+    try:
+        copyfolder(directory_pwd+file_test, directory_pwd+directory_test+file_test)        
+        assert os.path.isfile(directory_pwd+directory_test+file_test) is True
 
-    #except OSError as e:
-        #if e.errno != errno.EEXIST:
-            #raise
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
         
-    #except RuntimeError:
-         #raise
+    except RuntimeError:
+         raise
 
 
-def test_with_app():
-    @with_app
+#def test_with_app():
+    #@with_app
     
-    def mock_fun():
-        return True
+    #def mock_fun():
+        #return True
     
-    assert mock_fun.decorated is True
+    #assert mock_fun.decorated is True
 
 def test_check():
     pass
