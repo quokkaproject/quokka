4b28d7fb2a3851cc3386ae56d57f80eb0e15e3bb
marcosptf <marcosptf@yahoo.com.br>
2018-08-18 18:09:06 -0300

    code refactory add pytest-fixtures to =>tests/admin/test_formatters.py

--- tests/admin/test_formatters.py
+++ tests/admin/test_formatters.py
@@ -4,8 +4,16 @@ from flask import current_app as app
 from flask_htmlbuilder.htmlbuilder import html
 from quokka.core.content.models import make_model
 from quokka.admin.formatters import format_datetime, format_view_on_site, format_ul, format_link, format_status, format_url, format_custom_vars
-                                
 
+################################
+#pytest - fixtures - setUp();  #
+################################
+
+
+
+#####################################################
+#pytest - Quokka - quokka/admin/test_formatters.py  #
+#####################################################
 def test_format_datetime():
     pass
 
