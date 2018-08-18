a1e37dfbbe9af2211fc2014502c8922736fe3d95
marcosptf <marcosptf@yahoo.com.br>
2018-08-18 15:34:34 -0300

    add pytests to quokka/admin/test_formatters.py

--- tests/admin/test_formatters.py
+++ tests/admin/test_formatters.py
@@ -3,6 +3,30 @@ import mock
 from flask import current_app as app
 from flask_htmlbuilder.htmlbuilder import html
 from quokka.core.content.models import make_model
-from quokka.admin.formatters import format_datetime, format_view_on_site, format_ul, \
-                                    format_link, format_status, format_url, format_custom_vars
+from quokka.admin.formatters import format_datetime, format_view_on_site, format_ul, format_link, format_status, format_url, format_custom_vars
                                 
+
+def test_format_datetime():
+    pass
+
+def test_format_view_on_site():
+    pass
+
+def test_format_ul():
+    pass
+
+def test_format_link():
+    pass
+
+def test_format_status():
+    pass
+
+def test_format_url():
+    pass
+
+def test_format_custom_vars():
+    pass
+
+
+
+
