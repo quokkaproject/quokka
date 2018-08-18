eb1b11a0cf0574c234eeb1862e89afab9e16561f
marcosptf <marcosptf@yahoo.com.br>
2018-08-18 18:59:09 -0300

    fixing pep8 identing

--- tests/admin/test_formatters.py
+++ tests/admin/test_formatters.py
@@ -3,7 +3,9 @@ import mock
 from flask import current_app as app
 from flask_htmlbuilder.htmlbuilder import html
 from quokka.core.content.models import make_model
-from quokka.admin.formatters import format_datetime, format_view_on_site, format_ul, format_link, format_status, format_url, format_custom_vars
+from quokka.admin.formatters import format_datetime, \
+    format_view_on_site, format_ul, format_link, \
+    format_status, format_url, format_custom_vars
 
 ################################
 #pytest - fixtures - setUp();  #
