31c72bcdcabc155422f05c1df0dd669961106d4b
marcosptf <marcosptf@yahoo.com.br>
2018-09-20 09:05:31 -0300

    pytest:tests/core/__init__.py

--- tests/core/__init__.py
+++ tests/core/__init__.py
@@ -7,8 +7,27 @@ from quokka.core.content.views import (
     AuthorListView
 )
 from quokka.core.content.utils import url_for_content, strftime
+from quokka.core.content import configure
+from quokka import create_app
+
+#WIP:
+"""
+needs to see who use it
+import mock
+import pytest
+from quokka.core.app import QuokkaModule
+from quokka.core.content.admin import AdminArticlesView, AdminPagesView, AdminBlocksView
+from quokka.core.content.views import (
+    DetailView, PreviewView, ArticleListView, CategoryListView, TagListView,
+    AuthorListView
+)
+from quokka.core.content.utils import url_for_content, strftime
+from quokka.core.content import configure
+from quokka import create_app
+from quokka.core.content import configure
 
 
+"""
 def test_configure():
     pass
 
