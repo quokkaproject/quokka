66f4d0742ea7cddd02a206a9516c87c7ca9ff124
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:56:29 -0300

    pytest tests/core/test_themes.py

---
+++ tests/core/test_themes.py
@@ -0,0 +1,13 @@
+import jinja2
+from flask import send_from_directory
+from pathlib import Path
+from quokka.core.themes import configure, QuokkaTemplateLoader
+
+def test_configure():
+    pass
+
+
+def test_class_QuokkaTemplateLoader():
+    pass
+
+
