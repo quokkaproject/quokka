7745717b387e91f799acc9c25397794dc86533f9
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 17:05:58 -0300

    fixing imports tests/core/test_context_processors.py

--- tests/core/test_context_processors.py
+++ tests/core/test_context_processors.py
@@ -1,6 +1,7 @@
+import pytest
 import mock
 import click
-from .content.models import make_model, Category
+from quokka.core.content.models import make_model, Category
 from quokka.utils.blocks import build_menu, get_text_block, get_quokka_home
 from quokka.core.context_processors import configure
 
