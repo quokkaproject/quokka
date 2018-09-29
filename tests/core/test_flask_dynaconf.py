4250c0c6bbeeb266034bbb0bde1b40e740430f22
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:54:38 -0300

    pytest tests/core/test_flask_dynaconf.py

---
+++ tests/core/test_flask_dynaconf.py
@@ -0,0 +1,12 @@
+import mock
+import pytest
+from flask import Markup
+from dynaconf.contrib import FlaskDynaconf
+from dynaconf.utils import DynaconfDict
+from dynaconf.loaders import yaml_loader, env_loader, default_loader
+
+
+def test_configure_dynaconf():
+    pass
+
+
