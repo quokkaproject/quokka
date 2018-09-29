45d369a8c17d7654ddb42b89bf9467939e199d6a
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 17:01:34 -0300

    pytest tests/utils/test_custom_vars.py

---
+++ tests/utils/test_custom_vars.py
@@ -0,0 +1,13 @@
+import pytest
+import mock
+from dynaconf.utils.parse_conf import parse_conf_data
+from quokka.utils.custom_vars import parse_data, custom_var_dict
+
+def test_parse_data():
+    pass
+
+def test_custom_var_dict():
+    pass
+
+
+
