54dd97c3ca68612ec31e3e6ae55e4ffeae2a4c38
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:55:35 -0300

    pytest tests/core/test_regex_url_support.py

---
+++ tests/core/test_regex_url_support.py
@@ -0,0 +1,15 @@
+import pytest
+import mock
+from werkzeug.routing import BaseConverter
+from quokka.core.regex_url_support import RegexConverter, Regex
+
+def test_class_RegexConverter():
+    pass
+
+
+
+def test_class_Regex():
+    pass
+
+
+
