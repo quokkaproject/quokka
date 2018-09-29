876015f5ac7e292ab24523d08eae7da4c5eebe48
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 17:02:24 -0300

    tests/utils/test_project.py

---
+++ tests/utils/test_project.py
@@ -0,0 +1,17 @@
+import pytest
+import mock
+from .echo import green, lecho, red
+
+
+def test_fetch_theme():
+    pass
+
+
+def test_fetch_modules():
+    pass
+
+
+def test_cookiecutter():
+    pass
+
+
