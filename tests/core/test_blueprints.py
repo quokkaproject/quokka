282a43de84c38804e5d9e17850a8cc06666e31e4
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:52:59 -0300

    pytest tests/core/test_blueprints.py

---
+++ tests/core/test_blueprints.py
@@ -0,0 +1,16 @@
+import mock
+import click
+import importlib
+import os
+from quokka.core.commands_collector import CommandsCollector
+from quokka.core.blueprints import load_from_folder, get_blueprint_commands, blueprint_commands
+
+def test_load_from_folder():
+    pass
+
+def test_get_blueprint_commands():
+    pass
+
+def test_blueprint_commands():
+    pass
+
