b07f2e7e12c8557675034a64af750564f25bc3b2
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:49:08 -0300

    pytest tests/core/views/test_sitemap.py

---
+++ tests/core/views/test_sitemap.py
@@ -0,0 +1,15 @@
+import mock
+import click
+from flask import render_template
+from flask import current_app as app
+from flask.views import MethodView
+from quokka.core.content.models import make_model
+from quokka.core.views.sitemap import SiteMapView
+
+
+
+def test_class_SiteMapView():
+    pass
+
+
+
