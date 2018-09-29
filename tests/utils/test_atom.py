8301cb64472b1b29f7e26a83f911560892a4f94a
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:59:42 -0300

    pytest tests/utils/test_atom.py

---
+++ tests/utils/test_atom.py
@@ -0,0 +1,40 @@
+import pytest
+import mock
+from datetime import datetime
+from werkzeug._compat import implements_to_string, string_types
+from werkzeug.wrappers import BaseResponse
+
+
+
+################################
+#pytest - fixtures - setUp();  #
+################################
+XHTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
+
+
+
+
+##################################
+#pytest - Quokka - test_atom.py  #
+##################################
+def test_escape():
+    pass
+
+
+
+def test_make_text_block():
+    pass
+
+def test_format_iso8601():
+    pass
+
+#@implements_to_string
+def test_class_AtomFeed():
+    pass
+
+#@implements_to_string
+def test_class_FeedEntry():
+    pass
+
+
+
