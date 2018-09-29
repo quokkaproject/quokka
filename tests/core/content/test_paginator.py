db6cbfb3e13cd5dbb8112ccb85ea67c2c8921408
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:50:24 -0300

    pytest tests/core/content/test_paginator.py

---
+++ tests/core/content/test_paginator.py
@@ -0,0 +1,36 @@
+import mock
+import click
+import functools
+import logging
+import os
+from collections import namedtuple
+from math import ceil
+from flask import current_app as app
+import six
+from quokka.core.content.paginator import Paginator
+
+
+################################################################################
+#pytest - fixtures                                                             #
+################################################################################
+logger = logging.getLogger(__name__)
+PaginationRule = namedtuple(
+    'PaginationRule',
+    'min_page URL SAVE_AS',
+)
+
+DEFAULT_PP = [(1, '{name}/', '{name}/index{extension}'),
+              (2, '{name}/{number}/', '{name}/{number}/index{extension}')]
+
+
+#################################################################################
+#pytest - Quokka - test_paginator.py                                            #
+#################################################################################
+def test_class_Paginator():
+    pass
+
+def test_class_Page():
+    pass
+
+
+
