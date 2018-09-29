ea9c01d7dfa02ecd262918a3abd3b0f0036fa419
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:50:49 -0300

    pytest tests/core/content/test_parsers.py

---
+++ tests/core/content/test_parsers.py
@@ -0,0 +1,21 @@
+import mock
+import click
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
+from quokka.core.content.parsers import block_code, HighlightMixin, HighlightRenderer
+
+
+def test_block_code():
+    pass
+
+def test_class_HighlightMixin():
+    pass
+
+
+def test_class_HighlightRenderer():
+    pass
+
+
+
