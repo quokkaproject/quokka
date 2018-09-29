02fdd914199eec5d9cf1ece9cf9a8e7afea7c51d
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:52:04 -0300

    pytest tests/core/test_app.py

---
+++ tests/core/test_app.py
@@ -0,0 +1,13 @@
+import mock
+import click
+from flask import Blueprint, Flask
+from flask.helpers import _endpoint_from_view_func
+from quokka.core.app import QuokkaApp, QuokkaModule
+
+def test_class_QuokkaApp():
+    pass
+    
+
+def test_class_QuokkaModule():
+    pass
+    
