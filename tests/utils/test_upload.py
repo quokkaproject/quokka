2d0c39260f3230865891ded440caf070113cf730
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 17:03:22 -0300

    pytest tests/utils/test_upload.py

---
+++ tests/utils/test_upload.py
@@ -0,0 +1,21 @@
+import pytest
+import mock
+import os.path as op
+import random
+from datetime import date
+from flask import current_app
+from speaklater import make_lazy_string
+from werkzeug import secure_filename
+
+
+def test_dated_path():
+    pass
+
+def media_path():
+    pass
+
+
+def lazy_media_path():
+    pass
+
+ 
