8b78ec9ad44d3dcae638f36f07c4f779972ae332
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:51:22 -0300

    pytest  tests/core/content/test_utils.py

---
+++ tests/core/content/test_utils.py
@@ -0,0 +1,16 @@
+import mock
+import click
+from quokka.utils.text import slugify_category, slugify
+from flask import current_app as app
+from quokka.core.content.utils import url_for_content, url_for_category, strftime
+
+def test_url_for_content():
+    pass
+
+
+def test_url_for_category():
+    pass
+
+def test_strftime():
+    pass
+
