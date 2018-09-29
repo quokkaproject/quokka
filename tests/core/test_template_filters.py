77326bbf27d0d08ec3147fd74210c40a9e2848bb
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:56:10 -0300

    pytest tests/core/test_template_filters.py

---
+++ tests/core/test_template_filters.py
@@ -0,0 +1,10 @@
+from quokka.utils.blocks import get_block, get_blocks, get_block_by_id
+from quokka.core.template_filters import is_list, configure
+
+def test_is_list():
+    pass
+
+
+def test_configure():
+    pass
+
