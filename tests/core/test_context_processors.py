f3034696b3b21b12857092f5dbb202aae89f0b16
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:53:30 -0300

    pytest tests/core/test_context_processors.py

---
+++ tests/core/test_context_processors.py
@@ -0,0 +1,10 @@
+import mock
+import click
+from .content.models import make_model, Category
+from quokka.utils.blocks import build_menu, get_text_block, get_quokka_home
+from quokka.core.context_processors import configure
+
+def test_configure():
+    pass
+
+
