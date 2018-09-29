657fc0cbb155b0bc7edaaceb0e7755d59b962e26
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:53:15 -0300

    pytest tests/core/test_commands_collector.py

---
+++ tests/core/test_commands_collector.py
@@ -0,0 +1,15 @@
+import mock
+import click
+import importlib
+import os
+import sys
+import click
+from quokka.core.commands_collector import CommandsCollector
+
+def test_class_CommandsCollector():
+    pass
+
+
+
+
+
