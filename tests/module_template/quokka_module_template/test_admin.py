d87470dd1d48c6155774016908a6af40d2687c59
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:49:31 -0300

    pytest tests/module_template/quokka_module_template/

---
+++ tests/module_template/quokka_module_template/test_admin.py
@@ -0,0 +1,26 @@
+import pytest
+import mock
+from flask import current_app
+from flask_admin.contrib.pymongo import filters
+from flask_admin.form import Select2Widget
+from flask_admin.model.fields import InlineFieldList, InlineFormField
+from quokka.admin.forms import Form, fields
+from quokka.admin.views import ModelView
+from quokka.module_template.quokka_module_template.admin import InnerForm, UserForm, UserView, TweetForm
+
+
+def test_class_InnerForm():
+    pass
+
+def test_class_UserForm():
+    pass
+
+def test_class_UserView():
+    pass
+
+def test_class_TweetForm():
+    pass
+
+def test_class_TweetView():
+    pass
+
