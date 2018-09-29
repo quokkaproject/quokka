0e3304127def148c9f8a03563ca4d8a24f7fc090
marcosptf <marcosptf@yahoo.com.br>
2018-09-29 16:59:59 -0300

    pytest tests/utils/test_blocks.py

---
+++ tests/utils/test_blocks.py
@@ -0,0 +1,30 @@
+from flask import current_app
+from quokka.core.content.models import make_model
+from quokka.utils.blocks import get_block
+
+def test_get_block():
+    pass
+
+def test_get_block_by_id():
+    pass
+
+
+def test_get_blocks():
+    pass
+
+
+def test_build_menu():
+    pass
+
+
+def test_build_menu_item():
+    pass
+
+
+def test_get_text_block():
+    pass
+
+
+def test_get_quokka_home():
+    pass
+
