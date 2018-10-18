a636bd5152e244e34764b0e1f6c6ec04fe003196
marcosptf <marcosptf@yahoo.com.br>
2018-10-18 07:24:43 -0300

    remove import unused

--- tests/admin/test_widgets.py
+++ tests/admin/test_widgets.py
@@ -2,7 +2,6 @@ import pytest
 import mock
 import json
 from flask import current_app
-#from quokka.core.template import render_template
 from wtforms.widgets import TextArea, TextInput
 from quokka.admin.widgets import TextEditor, PrepopulatedText
 
