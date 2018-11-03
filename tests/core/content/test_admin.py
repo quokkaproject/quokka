68ee2f1c0e688d0e72ce4dd4954e209e27b6a703
marcosptf <marcosptf@yahoo.com.br>
2018-11-03 10:13:02 -0200

    add pytest headers to tests/core/content/test_formats_.py and remove comments fromb/tests/core/content/test_admin.py

--- tests/core/content/test_admin.py
+++ tests/core/content/test_admin.py
@@ -6,13 +6,9 @@ import pymongo
 from flask import current_app
 from quokka.admin.forms import ValidationError, rules
 from quokka.admin.views import ModelView, RequiresLogin
-#from quokka.admin.formatters import (
-#    format_datetime, format_view_on_site, format_custom_vars
-#)
 from quokka.core.auth import get_current_user
 from quokka.utils.text import slugify, slugify_category
 from quokka.core.content.formats import CreateForm, get_format
-
 from quokka.core.content.utils import url_for_content
 from quokka.core.content.admin import AdminContentView, AdminArticlesView, AdminPagesView, AdminBlocksView
 from flask_admin.contrib.pymongo import ModelView
@@ -22,7 +18,6 @@ from quokka.admin.actions import CloneAction, PublishAction
 ################################################################################
 #pytest - fixtures                                                             #
 ################################################################################
-
 class TestClassPytestExtendsAdminContentView(AdminContentView):
     def mock_init_method(self):
         return super(AdminContentView, self)
