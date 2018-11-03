d13c501ddc30161f9537e2836e128034dddb47f3
marcosptf <marcosptf@yahoo.com.br>
2018-11-03 18:30:39 -0200

    pytest - tests/core/content/test_formats.py - done

--- tests/core/content/test_formats.py
+++ tests/core/content/test_formats.py
@@ -32,6 +32,12 @@ from quokka.core.content.formats import (
 ################################################################################
 mock_obj = {'content_format' : 'debugger'}
 
+class MockFields():
+    data = "mock,url,to,pytest"
+
+mock_fields = MockFields()
+
+
 
 #################################################################################
 #pytest - Quokka - tests/core/content/test_formats_.py                          #
@@ -138,65 +144,348 @@ def test_validate_category_def_instance_error_outside_context():
             raise
 
 
-def test_get_category_kw():
-    pass
+def test_get_category_kw_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_category_kw(fields)
+            assert "Working outside of application context." in str(err.value)
 
-def test_validate_block_item():
-    pass
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
 
-def get_block_item_kw():
-    pass
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
-def test_get_default_category():
-    pass
+        except FileExistsError:
+            raise        
 
-def test_get_authors_kw():
-    pass
-
-def test_get_default_author():
-    pass
-
-def test_get_tags_kw():
-    pass
-
-def test_get_default_language():
-    pass
-
-def test_BaseForm():
-    pass
-
-def test_CreateForm():
-    pass
-
-def test_CustomVariablesForm():
-    pass
-
-def test_BlockItemForm():
-    pass
-
-def test_BaseEditForm():
-    pass
-
-def test_BaseFormat():
-    pass
-
-def test_PlainEditForm():
-    pass
+        except Exception:
+            raise
 
 
-def test_PlainFormat():
-    pass
+def test_validate_block_item_def_should_retur_error_message_notice():
+    assert validate_block_item(None, mock_fields) == "You can select only one URL for each item"
 
-def test_HTMLEditForm():
-    pass
 
-def test_HTMLFormat():
-    pass
+def test_get_block_item_kw_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_block_item_kw(fields)
+            assert "Working outside of application context." in str(err.value)
 
-def test_MarkdownEditForm():
-    pass
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
 
-def test_MarkdownFormat():
-    pass
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise        
+
+        except Exception:
+            raise
+
+
+def test_get_default_category_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_default_category()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise        
+
+        except Exception:
+            raise
+
+
+def test_get_authors_kw_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_authors_kw(fields)
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise        
+
+        except Exception:
+            raise
+
+
+def test_get_default_author_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_default_author()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_get_tags_kw_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_tags_kw(fields)
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_get_default_language_def_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            get_default_language()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_BaseForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = BaseForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_CreateForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = CreateForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_CustomVariablesForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = CustomVariablesForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+
+def test_BlockItemForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = BlockItemForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_BaseEditForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = BaseEditForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_BaseFormat_class_instance_error_outside_context():
+    with pytest.raises(TypeError) as err:
+        try:
+            trying_make_a_instance = BaseFormat(BaseForm)
+            assert "takes no parameters" in str(err.value)
+
+        except AttributeError as atterr:
+            assert "object has no attribute" in str(atterr.value)
+
+        except RuntimeError as e:
+            raise
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_PlainEditForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        
+        
+        try:
+            trying_make_a_instance = PlainEditForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_PlainFormat_class_instance_is_same_PlanEditForm():
+    pf = PlainFormat()
+    assert (type(pf.edit_form) == type(PlainEditForm)) is True
+
+def test_HTMLEditForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = HTMLEditForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+def test_HTMLFormat_class_isinstance_of_HTMLFormat():
+    html = HTMLFormat()
+    assert isinstance(html, HTMLFormat) is True
+
+
+def test_MarkdownEditForm_class_instance_error_outside_context():
+    with pytest.raises(RuntimeError) as err:
+        try:
+            trying_make_a_instance = MarkdownEditForm()
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
+def test_MarkdownFormat_class_isinstance_of_MarkdownFormat():
+    mf = MarkdownFormat()
+    assert isinstance(mf, MarkdownFormat) is True
+
+
 
 
