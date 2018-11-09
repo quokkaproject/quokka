20c8b159786ead54c77a712ccde2103463349b02
marcosptf <marcosptf@yahoo.com.br>
2018-11-09 07:42:06 -0200

    pytest tests/core/views/test_sitemap.py

--- tests/core/views/test_sitemap.py
+++ tests/core/views/test_sitemap.py
@@ -7,9 +7,25 @@ from quokka.core.content.models import make_model
 from quokka.core.views.sitemap import SiteMapView
 
 
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+sitemapview = SiteMapView()
 
-def test_class_SiteMapView():
-    pass
 
+#######################################################
+#pytest - Quokka - tests/core/views/test_sitemap.py   #
+#######################################################
+def test_class_SiteMapView_isinstance():
+    assert isinstance(sitemapview, SiteMapView) is True
+
+def test_class_Sitemap_decorators_empty_tuple():
+    assert sitemapview.decorators == ()
+
+def test_class_Sitemap_mehots_dicionary_GET():    
+    assert  sitemapview.methods == {'GET'}
+
+def test_class_Sitemap_provide_automatic_options_property_is_None():
+    assert sitemapview.provide_automatic_options is None
 
 
