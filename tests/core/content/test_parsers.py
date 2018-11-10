5d201a74edc72d317f6425d42997b6afff870dfd
marcosptf <marcosptf@yahoo.com.br>
2018-11-10 11:59:55 -0200

    pytest - tests/core/content/test_parsers.py

--- tests/core/content/test_parsers.py
+++ tests/core/content/test_parsers.py
@@ -1,17 +1,49 @@
+import pytest
 import mock
 import click
 import mistune
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import html
-from quokka.core.content.parsers import block_code, HighlightMixin, HighlightRenderer
+from quokka.core.content.parsers import (
+    block_code,
+    HighlightMixin,
+    HighlightRenderer
+)
 
 
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+block = block_code(text="java-debugger-code", lang="pt")
+
+#######################################################
+#pytest - Quokka - tests/core/content/test_parsers.py #
+#######################################################
 def test_block_code():
-    pass
+    assert block == '<pre class="pt"><code>java-debugger-code</code></pre>\n'
 
 def test_class_HighlightMixin():
-    pass
+
+    with pytest.raises(AttributeError) as err:
+        try:
+            high = HighlightMixin()
+            block = high.block_code(text="java-debugger-code", lang="pt")
+            assert "HighlightMixin object has no attribute options" in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
 
 
 def test_class_HighlightRenderer():
