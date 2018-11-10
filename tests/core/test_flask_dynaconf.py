bea9523cb4fb97b4bfa7a459629961c6afe2f4a4
marcosptf <marcosptf@yahoo.com.br>
2018-11-10 16:36:17 -0200

    pytest - tests/core/test_monitoring.py

--- tests/core/test_flask_dynaconf.py
+++ tests/core/test_flask_dynaconf.py
@@ -4,9 +4,21 @@ from flask import Markup
 from dynaconf.contrib import FlaskDynaconf
 from dynaconf.utils import DynaconfDict
 from dynaconf.loaders import yaml_loader, env_loader, default_loader
+from quokka import create_app
+from quokka.core.flask_dynaconf import configure_dynaconf
 
 
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+app = create_app(test=True)
+
+
+#######################################################
+#pytest - Quokka - tests/core/views/test_sitemap.py   #
+#######################################################
 def test_configure_dynaconf():
-    pass
+    conf = configure_dynaconf(app)
+    assert conf is None
 
 
--- tests/core/test_logger.py
+++ tests/core/test_logger.py
@@ -1,9 +1,21 @@
 import pytest
 import mock
 import logging
+from quokka.core.logger import configure
+from quokka import create_app
 
 
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+app = create_app(test=True)
+
+
+#######################################################
+#pytest - Quokka - tests/core/views/test_sitemap.py   #
+#######################################################
 def test_configure():
-    pass
+    conf = configure(app)
+    assert conf is None
 
 
