ca58975877047bbaf0ac33efd802f5279247c6e1
marcosptf <marcosptf@yahoo.com.br>
2018-11-10 11:45:58 -0200

    pytest - tests/core/content/test_utils.py

--- tests/core/content/test_utils.py
+++ tests/core/content/test_utils.py
@@ -1,16 +1,50 @@
+import time
+import pytest
 import mock
 import click
-from quokka.utils.text import slugify_category, slugify
+from quokka.utils.text import (
+    slugify_category, slugify
+)
 from flask import current_app as app
-from quokka.core.content.utils import url_for_content, url_for_category, strftime
+from quokka.core.content.utils import (
+    url_for_content,
+    url_for_category,
+    strftime
+)
 
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+param_dict = {"java":"debugger", "slug":"slug-mock", "title":"title-mock"}
+
+
+#######################################################
+#pytest - Quokka - tests/core/content/test_utils.py   #
+#######################################################
 def test_url_for_content():
-    pass
 
+    with pytest.raises(RuntimeError) as err:
+        try:
+            url = url_for_content(content=param_dict)
+            assert "Working outside of application context." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
 
 def test_url_for_category():
-    pass
+    url = url_for_category("java-categoty-mock")
+    assert url == "java-categoty-mock"
 
 def test_strftime():
-    pass
+    assert strftime(time, "%Y") == "2018"
 
--- tests/core/content/test_views.py
+++ tests/core/content/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 import mock
 import click
 import hashlib
