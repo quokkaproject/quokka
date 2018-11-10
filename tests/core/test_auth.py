40edb96577d504188f84d02e6266e74149e28301
marcosptf <marcosptf@yahoo.com.br>
2018-11-10 14:38:27 -0200

    pytest - tests/core/test_auth.py

--- tests/core/test_auth.py
+++ tests/core/test_auth.py
@@ -1,18 +1,66 @@
+import pytest
 import mock
 import click
 import getpass
-from flask import current_app, url_for, Markup
+from flask import (
+    current_app, url_for, Markup
+)
 from flask_htmlbuilder.htmlbuilder import html
-from quokka.admin.actions import UserProfileBlockAction
-from quokka.admin.views import ModelView
-from quokka.admin.forms import Form, fields, ValidationError, validators
-from quokka.utils.text import slugify
-from werkzeug.security import check_password_hash, generate_password_hash
-from flask_simplelogin import SimpleLogin, get_username
-from quokka.core.auth import create_user
+from quokka.admin.actions import (
+    UserProfileBlockAction
+)
+from quokka.admin.views import (
+    ModelView
+)
+from quokka.admin.forms import (
+    Form, fields, 
+    ValidationError, validators
+)
+from quokka.utils.text import (
+    slugify
+)
+from werkzeug.security import (
+    check_password_hash, 
+    generate_password_hash
+)
+from flask_simplelogin import (
+    SimpleLogin, get_username
+)
+from quokka.core.auth import (
+    create_user, UserForm,
+    format_profile, UserView,
+    validate_login, configure,
+    configure_user_admin, get_current_user
+)
 
+################################################################################
+#pytest - fixtures                                                             #
+################################################################################
+param_data = {'username': 'mock-user', 'password':'mock-pass'}
+
+
+#################################################################################
+#pytest - Quokka - tests/core/test_app.py                                       #
+#################################################################################
 def test_create_user():
-    pass
+
+    with pytest.raises(ValueError) as err:
+        try:
+            debugger = create_user(data=param_data)
+            assert "username and password are required." in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise        
+
+        except Exception:
+            raise
 
 
 def test_class_UserForm():
