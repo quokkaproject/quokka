015181755ff332d149b823aa21dd2cf908cb2aed
marcosptf <marcosptf@yahoo.com.br>
2018-11-10 16:22:48 -0200

    tests/core/test_error_handlers.py

--- tests/core/test_db.py
+++ tests/core/test_db.py
@@ -11,14 +11,38 @@ from tinymongo.tinymongo import generate_id
 from quokka.utils.text import split_all_category_roots
 from quokka.core.db import QuokkaTinyMongoClient, QuokkaDB
 
-def test_class_QuokkaTinyMongoClient():
-    pass
+
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+quokka_mongo = QuokkaTinyMongoClient()
+quokka_db = QuokkaDB()
+mock_collections = {'index': 'index', 'contents': 'contents', 'uploads': 'uploads', 'users': 'users'}
+
+#######################################################
+#pytest - Quokka - tests/core/views/test_sitemap.py   #
+#######################################################
+def test_class_QuokkaTinyMongoClient_isinstance():
+    assert isinstance(quokka_mongo, QuokkaTinyMongoClient) == True
 
 
-def test_class_QuokkaDB():
-    pass
+def test_class_QuokkaDB_isinstance():
+    assert isinstance(quokka_db, QuokkaDB) == True
+
+def test_class_QuokkaDB_collections():
+    assert quokka_db.collections == mock_collections
+
+def test_class_QuokkaDB_config():
+    assert quokka_db.config == {}
+
+def test_class_QuokkaDB_folder():
+    assert quokka_db.folder == 'databases'
+
+def test_class_QuokkaDB_host():
+    assert quokka_db.host == 'localhost'
+
+def test_class_QuokkaDB_name():
+    assert quokka_db.name == 'quokka_db'
 
 
-def test_is_equal():
-    pass
 
--- tests/core/test_error_handlers.py
+++ tests/core/test_error_handlers.py
@@ -1,9 +1,20 @@
 import mock
 import click
 from flask import render_template
+from quokka import create_app
+from quokka.core.error_handlers import configure
+
+#######################################################
+#pytest - fixtures                                    #
+#######################################################
+app = create_app(test=True)
 
 
+#######################################################
+#pytest - Quokka - tests/core/views/test_sitemap.py   #
+#######################################################
 def test_configure():
-    pass
+    assert configure(app) is None
+
 
 
