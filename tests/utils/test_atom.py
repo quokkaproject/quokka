b4a6632545fdf7d64fc61943cab5b36c630dafa3
marcosptf <marcosptf@yahoo.com.br>
2018-11-11 12:11:04 -0200

    pytest - tests/utils/test_atom.py

--- tests/utils/test_atom.py
+++ tests/utils/test_atom.py
@@ -1,11 +1,15 @@
 import pytest
 import mock
+import time
 from datetime import datetime
-from werkzeug._compat import implements_to_string, string_types
+from werkzeug._compat import (
+    implements_to_string, string_types
+)
 from werkzeug.wrappers import BaseResponse
 from quokka.utils.atom import (
     escape, _make_text_block,
-    AtomFeed, FeedEntry
+    AtomFeed, FeedEntry, 
+    format_iso8601
 )
 
 
@@ -14,6 +18,8 @@ from quokka.utils.atom import (
 ################################
 XHTML_NAMESPACE = 'http://www.w3.org/1999/xhtml'
 make = _make_text_block(name="name-param-mock", content="content-param-mock")
+param_mock = {"title_type":"", "text":"", "url":"", "feed_url":"", "id":"", "updated":"",
+"author":"", "icon":"", "logo":"", "rights":"", "rights_type":"", "subtitle":"", "subtitle_type":"", "generator":"", "links":""}
 
 
 ##################################
@@ -27,15 +33,51 @@ def test_make_text_block():
 
 
 def test_format_iso8601():
-    pass
+    format=format_iso8601(time)
+    assert format != ""
 
-#@implements_to_string
+
 def test_class_AtomFeed():
-    pass
 
-#@implements_to_string
+    with pytest.raises(ValueError) as err:
+        try:
+            atom = AtomFeed(title="title-mock", entries="entries-mock", kwargs=param_mock)
+            assert "id is required" in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
+
 def test_class_FeedEntry():
-    pass
+
+    with pytest.raises(ValueError) as err:
+        try:
+            atom = FeedEntry(title="title-mock", entries="entries-mock", kwargs=param_mock)
+            assert "id is required" in str(err.value)
+
+        except TypeError as e:
+            assert 'nargs=-1' in str(e)
+
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        except FileExistsError:
+            raise
+
+        except Exception:
+            raise
+
 
 
 
